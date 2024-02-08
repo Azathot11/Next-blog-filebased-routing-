032ac1249c1cbd8b09bf9897af41c4f855ba733c
Azathot11 <azathotkamdoum@gmail.com>
2024-02-08 15:45:16 +0100

    completed small blog project

--- pages/events/[...slug].jsx
+++ pages/events/[...slug].jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import {getFilteredEvents} from "../../utils/dummy-data";
 import EventList from "../../components/events/EventList";
+import ResultsTitle from "../../components/events/results-title";
+import Button from "../../components/ui/Button";
+import ErrorAlert from "../../components/ui/error-alert";
 
 const FilteredEvents = () => {
     const { query } = useRouter();
@@ -14,18 +17,35 @@ const FilteredEvents = () => {
     const filteredYear =  +filteredData[0];
     const filteredMonth = + filteredData[1];
 
-    if(isNaN(filteredYear) || isNaN(filteredMonth) || filteredYear > 2030 || filteredYear < 2021 || filteredMonth < 1 || filteredMonth > 12) {
-        return <p className='center'> Invalid filter please adjust your values</p>
+    if (isNaN(filteredYear) || isNaN(filteredMonth) || filteredYear > 2030 || filteredYear < 2021 || filteredMonth < 1 || filteredMonth > 12) {
+        return (
+            <>
+                <ErrorAlert className='center'> Invalid filter please adjust your values</ErrorAlert>
+                <div className={'center'}>
+                    <Button path='/events'>Show all events</Button>
+                </div>
+            </>
+        )
     }
 
-const filteredEvents  =getFilteredEvents({year:filteredYear,month:filteredMonth})
+    const filteredEvents = getFilteredEvents({year: filteredYear, month: filteredMonth})
     if (!filteredEvents || filteredEvents.length === 0) {
-        return <p className='center'>No events found for the chosen filter</p>
+        return(
+            <>
+                <ErrorAlert >No events found for the chosen filter</ErrorAlert>
+                <div className={'center'}>
+                <Button path='/events'>Show all events</Button>
+                </div>
+            </>
+        )
     }
+
+    const date = new Date(filteredYear, filteredMonth - 1);
     return (
-        <div>
+        <>
+            <ResultsTitle date={date}/>
             <EventList items={filteredEvents}></EventList>
-        </div>
+        </>
     );
 };
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import {getFeaturedEvents} from "../utils/dummy-data";
 import EventList from "../components/events/EventList";
 
-
 const Home  = () => {
 
     const featuredEvents =  getFeaturedEvents()
+
     return (
-        <div>
+        <>
                 <EventList items={featuredEvents} />
-        </div>
+        </>
     );
 };
 
