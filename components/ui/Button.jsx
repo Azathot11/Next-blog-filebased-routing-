9a52ddb9f8f633af591c5831599e7bb32fdb2a72
Azathot11 <azathotkamdoum@gmail.com>
2024-02-08 14:54:32 +0100

    filters

--- components/ui/Button.jsx
+++ components/ui/Button.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import Link from "next/link";
 import classes from './Button.module.css';
 
-const Button = ({path,children}) => {
+const Button = ({path,children,}) => {
+    if (!path){
+        return (
+            <button className={classes.btn} onClick={()=>{}}>{children}</button>
+        )
+    }
     return (
         <Link href={path} className={classes.btn}>
             {children}
--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import {getAllEvents} from "../../utils/dummy-data";
+import EventList from "../../components/events/EventList";
+import EventsSearch from "../../components/events/EventsSearch";
 
 const AllEvents = () => {
+    const events = getAllEvents()
     return (
         <div>
-            <h1>All Events</h1>
+            <EventsSearch/>
+            <EventList items={events}/>
         </div>
     );
 };
